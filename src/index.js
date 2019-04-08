81d913f69f59b2921b615983b48fdb8eeffe9d1f
ArnaVala <arnavala@gmail.com>
2019-04-08 14:25:04 +0300

    react install

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Minimal react boilerplate setup");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "Minimal React Boilerplate - AV";
+
+ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
